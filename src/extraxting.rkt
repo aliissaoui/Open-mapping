d02f7e1866f7d9f095b35abbcb69f5d661a6f7fb
Lebdaoui Selmane <Selmane.Lebdaoui@bordeaux-inp.fr>
2019-03-17 18:03:50 +0100

    cleaning the code

--- src/extraxting.rkt
+++ src/extraxting.rkt
@@ -3,16 +3,15 @@
 
 (define osm (xml->xexpr (document-element
     (read-xml (open-input-file "../maps/projMapping.osm")))))
-;(define (delete-vide-string liste)
-;   (cdr (cons list)))
-;(car (car (cdr (cddddr osm))))
+
+;;extraire la liste des champs de la liste l de type 'type ('node)
 (define (extract-champs f l type)
   (cond
     [(null? l) f]
     [(and (list? (car l)) (equal? (caar l) type))
                                                   (extract-champs  (cons (car l) f) (cdr l) type)]
     [else (extract-champs f (cdr l) type) ]))
-;
+
 ;(define (delete-unuseless f l)
 ;  (cond
 ;    [(null? l) f]
@@ -21,44 +20,46 @@
 ;     ((cons (delete-unuseless (car l) (delete-unuseless (cdr l) f))))]
 ;    [else (delete-unuseless f (cdr l))]))
 
-(define node-list (extract-champs '() osm 'node))
-(define way-list (extract-champs '() osm 'way))
-(define way-lists (extract-champs '() way-list 'id))
-;;(delete-unuseless way-list '())
-way-list
-;(caadar way-list);;id of the way
-(define a (cdaadr (cadddr (car way-list))));;id du premièr node  
-(define b (cdaadr (cadr (cddddr (car way-list)))));;id du dexième node 
-;;(cadddr (cddddr (car way-list)))
-(define way
-         '((way ((id "199797372")) "\n    " (nd ((ref "2097959544"))) "\n    " (nd ((ref "515330686"))) "\n    " (tag ((k "highway") (v "tertiary"))) "\n  ")
-           (way ((id "111")) "\n    " (nd ((ref "222"))) "\n    " (nd ((ref "11"))) "\n    " (tag ((k "highway") (v "tertiary"))) "\n  ")
-))
-(list a b);;la liste des deux nodes 
-(caadr (list a b));;reacceder au premier node 
+
+;;id-filter-way -> liste des id des nodes qui sont liés 
 (define (id-filter-way l f)
   (if (null? l)
       f
       (id-filter-way (cdr l) (cons (list (cdaadr (cadddr (car l))) (cdaadr (cadr (cddddr (car l))))) f) )))
- (id-filter-way way-list '())
-(id-filter-way way '())
+
+
+
+;; is-id? -> vrai si id est l'id du node l
 (define (is-id? id  l)
   (equal? (car id) (car (cdaadr l))))
 
-(define (add-neighbours l id f)
+;;add-neignbours --> retourne une liste des voisins selon le lien ids 
+(define (add-neighbours l ids f)
   (cond
     [(null? l) f]
-    [(is-id? (car id) (car l)) (list (add-neighbours (cdr l) id (cons (car l) (cadr id))) f)]
-    [(is-id? (cadr id) (car l)) (list (add-neighbours (cdr l) id (cons (car l) (car id))) f)]
-    [else (add-neighbours (cdr l) id (list (car l) f))]
+    [(is-id? (car ids) (car l)) (list (add-neighbours (cdr l) ids (cons (car l) (cadr ids))) f)]
+    [(is-id? (cadr ids) (car l)) (list (add-neighbours (cdr l) ids (cons (car l) (car ids))) f)]
+    [else (add-neighbours (cdr l) ids (list (car l) f))]
     )
   )
-(add-neighbours node-list '(("2097959544") ("515330686")) '())
-  
+;;construct-graphe construction du graphe à partir du fichier osm
 (define (construct-graphe file)
   (define way-list (extract-champs '() file 'way))
   (define node-list (extract-champs '() file 'node))
   (define neighbours-list (id-filter-way way-list '()))
   (add-neighbours node-list (car neighbours-list) '())
  )
-(construct-graphe osm)    
+
+
+;;tests
+(define node-list (extract-champs '() osm 'node))
+(define way-list (extract-champs '() osm 'way))
+(define way-lists (extract-champs '() way-list 'id))
+(define a (cdaadr (cadddr (car way-list))));;id du premièr node  
+(define b (cdaadr (cadr (cddddr (car way-list)))));;id du dexième node 
+(define way
+         '((way ((id "199797372")) "\n    " (nd ((ref "2097959544"))) "\n    " (nd ((ref "515330686"))) "\n    " (tag ((k "highway") (v "tertiary"))) "\n  ")
+           (way ((id "111")) "\n    " (nd ((ref "222"))) "\n    " (nd ((ref "11"))) "\n    " (tag ((k "highway") (v "tertiary"))) "\n  ")
+))
+(id-filter-way way-list '())
+(id-filter-way way '())
