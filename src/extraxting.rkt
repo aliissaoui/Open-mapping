ba6d6ff376e98aa47407e1a69ca542b3f6da9e12
Lebdaoui Selmane <Selmane.Lebdaoui@bordeaux-inp.fr>
2019-03-22 17:01:38 +0100

    improving the code

--- src/extraxting.rkt
+++ src/extraxting.rkt
@@ -12,28 +12,21 @@
                                                   (extract-champs  (cons (car l) f) (cdr l) type)]
     [else (extract-champs f (cdr l) type) ]))
 
-;(define (delete-unuseless f l)
-;  (cond
-;    [(null? l) f]
-;    [(string? (car l))]
-;    [(list? (car l))
-;     ((cons (delete-unuseless (car l) (delete-unuseless (cdr l) f))))]
-;    [else (delete-unuseless f (cdr l))]))
-
 
 ;;id-filter-way -> liste des id des nodes qui sont liés 
 (define (id-filter-way l f)
   (if (null? l)
       f
-      (id-filter-way (cdr l) (cons (list (cdaadr (cadddr (car l))) (cdaadr (cadr (cddddr (car l))))) f) )))
+      (id-filter-way (cdr l) (cons (list (string->number (car (cdaadr (cadddr (car l))))) (string->number (car (cdaadr (cadr (cddddr (car l))))))) f) )))
 
 
 
 ;; is-id? -> vrai si id est l'id du node l
 (define (is-id? id  l)
-  (equal? (car id) (car (cdaadr l))))
+  (equal? (car id) (car l)))
 
-;;add-neignbours --> retourne une liste des voisins selon le lien ids 
+;;add-neignbours --> retourne une liste des voisins selon le lien ids
+;;fonction à reparer
 (define (add-neighbours l ids f)
   (cond
     [(null? l) f]
@@ -45,14 +38,26 @@
 ;;construct-graphe construction du graphe à partir du fichier osm
 (define (construct-graphe file)
   (define way-list (extract-champs '() file 'way))
-  (define node-list (extract-champs '() file 'node))
+  (define node-list (parse-node (extract-champs '() file 'node)))
   (define neighbours-list (id-filter-way way-list '()))
-  (add-neighbours node-list (car neighbours-list) '())
+  (add-neighbours node-list neighbours-list '())
  )
-
-
-;;tests
+;;aramettres ; cdr d un champs node -> list des 3 champs 
+(define (parse1-node node)
+  (if (null? node)
+      '()
+      (cons (string->number (cadar node)) (parse1-node (cdr node)))))
+(define (parse-node nodes)
+  (cond
+    [(null? nodes) '()]
+    [(list? (car nodes)) (cons (parse1-node (cadar nodes)) (parse-node (cdr nodes)))]
+    [else (cons (cons (parse-node (cdar nodes))) (parse1-node (cdr nodes)))]))
 (define node-list (extract-champs '() osm 'node))
+
+(parse-node node-list)
+;(parse1-node '((id "515330686") (lat "44.7754213") (lon "-0.8587464")))
+;;tests
+;(define node-list (extract-champs '() osm 'node))
 (define way-list (extract-champs '() osm 'way))
 (define way-lists (extract-champs '() way-list 'id))
 (define a (cdaadr (cadddr (car way-list))));;id du premièr node  
