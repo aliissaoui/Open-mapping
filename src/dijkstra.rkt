0995ce6a40032dd4b48e2b277a14afb7b0feab07
BOUZIDI Imane <Imane.Bouzidi@bordeaux-inp.fr>
2019-04-25 15:23:41 +0200

    dijkstra

--- src/dijkstra.rkt
+++ src/dijkstra.rkt
@@ -1,11 +1,12 @@
 #lang racket
+(define dmax (expt 10 10)) 
 
 (define ht (make-hash))
 
 (struct vertex (id lat lon way))
 (struct graph (vx-ht)) 
 
-#|(define v1 (vertex 1 3.2 4.7 '(2 3)))
+(define v1 (vertex 1 3.2 4.7 '(2 3)))
 (define v2 (vertex 2 2.0 8.5 '(4 5 6 1)))
 (define v3 (vertex 3 1.5 9.1 '(7 8 1)))
 (define v4 (vertex 4 1.0 0.5 '(2)))
@@ -25,7 +26,7 @@
 (hash-set! ht 7 v7)
 (hash-set! ht 8 v8)
 (hash-set! ht 9 v9)
-|#
+
 
 
 (define (difference_latitude_deg s1 s2) ;; d1
@@ -54,11 +55,22 @@
 (haversine w v)
 |#
 
-(define (initialisation id_sommet liste_id_sommet l) ;d une liste initialement vide ; sommet est id ; liste_sommet est une liste des id des sommets du graph
+(define (initial id_sommet liste_id_sommet l) ;d une liste initialement vide ; sommet est id ; liste_sommet est une liste des id des sommets du graph
   (match liste_id_sommet
     ['() l]
     [(cons v tail)
-     (initialisation id_sommet tail (cons (cons v (if (eq? v id_sommet) 0  'infini)) l))]))
+     (initial id_sommet tail (cons (cons v (if (eq? v id_sommet) 0  1000000000000000))
+                                          l))]))
+
+(define (distance tab_dis s)
+ (cond [(null? tab_dis) (void)]
+    [(eq? (car (car tab_dis)) s)
+      (cdr (car tab_dis))]
+      [else (distance (cdr tab_dis) s)]
+      ))
+
+;(distance '((4 . 1000000000000000) (1 . 1000000000000000) (3 . 1000000000000000) (2 . 0)) 2)      
+
 
 (define v (vertex 2 5 1 '(5 6)))
 (define w (vertex 3 0 9 '(2 6)))
@@ -68,7 +80,7 @@
 (define l1 '(2 3 1 4))
 (define d '())
 
-(initialisation 2 l1 '())
+(initial 2 l1 '())
 
 
 (define (data id gr)              
@@ -76,15 +88,15 @@
 
 ;(vertex-way (hash-ref (graph-vx-ht g) 2)) 
 
-(define (min_dis liste_sommet sdep d smin)                                                  ;d contient initialement une valeur superieure aux distances   
-  (cond [(null? liste_sommet) (cons (vertex-id smin) d)]                                    ;séparant chaque deux points du graph, 10**36 par exemple et liste_sommet est une liste de vertex
+(define (min_dis liste_sommet sdep d smin)                                        ;d est initialement une très grande distance                                                    
+  (cond [(null? liste_sommet) (cons (vertex-id smin) d)]                          ;séparant chaque deux points du graph, 10**36 par exemple et liste_sommet est une liste de vertex
         [(and (<= (haversine sdep (car liste_sommet)) d) (not(eq? (car liste_sommet) sdep)))
          (min_dis (cdr liste_sommet) sdep (haversine sdep (car liste_sommet)) (car liste_sommet))]                   
         [else (min_dis (cdr liste_sommet) sdep d smin)]
         )
   )
 
-;(min_dis l v 100000000000000000 w)
+(min_dis l v 100000000000000000 w)
 
 
 (define (maj_tab_dis s d dis)                                                ;met à jour le tableau des distances
@@ -95,24 +107,70 @@
 
 ;(maj_dis 1 10 '((4 . infini) (1 . infini) (3 . infini) (2 . 0))) 
 
-(define (maj_pred pred s1 s2)                     ;le predecesseur de s2 est s1 (s1 s2) 
+(define (maj_pred pred s1 s2)                                               ;le predecesseur de s2 est s1 (s1 s2) 
   (cond [(eq? s2 (cdr (car pred)))
          (cons (list s1 s2) (cdr pred))]
         [else (maj_pred (cdr pred) s1 s2)])
         pred)
 
+(define pred '((v1 v2) (v2 v3) (v3 v4) (v5 v6) (v6 v7) (v7 v8) (v8 v9)))
 
-(define (maj_distance sdep s1 s2 pred dis )                               ;met à jour la valeur de la distance et le predecesseur
-  (if (> (haversine sdep s2) (+ (haversine sdep s1) (haversine s2 s1)))
-      (and (maj_tab_dis s2 (+ (haversine sdep s2) (haversine s2 s1)) dis)
-           (maj_pred s1 s2)) 
-      (void))
+
+#|
+(define (maj_distance sdep s1 s2 pred dis)                                  ;met à jour la valeur de la distance et le predecesseur
+  (if (> ((distance dis s2) (+ (distance dis s1) (haversine s2 s1))))
+     (cons (maj_tab_dis s2 (+ (haversine sdep s2) (haversine s2 s1)) dis)
+           (maj_pred s1 s2))
+  (cons dis pred))
   )
+
+(define (maj_dis_voisin sdep s1 lv pred dis)
+  (foldl (maj_distance sdep s1 pred dis) lv))     
+
+(define (dijkstra-loop q dis pred sdep)                                        ;q contient initialement la liste des id de tous les sommets
+  (if (null? q)
+      (cons dis pred)
+      ((let ((s1 (min_dis q sdep dmax sdep))
+            (q (remove s1 q))
+            (lv  (vertex-way (data s1 q ))))
+            (maj_dis_voisin sdep s1 lv)       
+       (maj_dis_voisin sdep s1 lv)
+       (dijkstra-loop q dis pred sdep)))))
+
+(define (dijkstra q sdep )
+  (let
+      ((dis (initial sdep q '()))
+       (pred '())
+       )
+    (dijkstra-loop q dis pred sdep)))
+
+
+
+  
+  
+  
+  
+    
+    
+  
+  
   
 
 
 
 
+Dijkstra(G,Poids,sdeb)
+1 Initialisation(G,sdeb)
+2 Q := ensemble de tous les nœuds
+3 tant que Q n'est pas un ensemble vide faire
+4       s1 := Trouve_min(Q)
+5       Q := Q privé de s1
+6       pour chaque nœud s2 voisin de s1 faire
+7           maj_distances(s1,s2)
+8       fin pour
+9 fin tant que
+
+|#
 
 
 
