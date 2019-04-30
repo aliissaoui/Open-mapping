24c1ff20b8d67914a60c180ea46e89ce0fbe6326
BOUZIDI Imane <Imane.Bouzidi@bordeaux-inp.fr>
2019-04-30 17:28:39 +0200

    dijkstra

--- src/dijkstra.rkt
+++ src/dijkstra.rkt
@@ -1,10 +1,7 @@
 #lang racket
-;(require "make_graph.rkt")
-(define dmax (expt 10 10)) 
-
 (require "hash-graph.rkt")
-(provide (all-defined-out))
-
+(require "make_graph.rkt")
+(define dmax (expt 10 10)) 
 
 
 (define (difference_latitude_deg s1 s2) ;; d1
@@ -40,23 +37,24 @@
      (initial id_sommet tail (cons (cons v (if (eq? v id_sommet) 0  1000000000000000))
                                           l))]))
 
-(define (distance tab_dis s)
- (cond [(null? tab_dis) (void)]
-    [(eq? (car (car tab_dis)) s)
-      (cdr (car tab_dis))]
-      [else (distance (cdr tab_dis) s)]
+(define (distance liste_double s)
+ (cond [(null? liste_double) (void)]
+    [(eq? (car (car liste_double)) s)
+      (cadar liste_double)]
+      [else (distance (cdr liste_double) s)]
       ))
 
 ;(distance '((4 . 1000000000000000) (1 . 1000000000000000) (3 . 1000000000000000) (2 . 0)) 2)      
 
 
-(define v (vertex 2 5 1 '(5 6)))
+#|(define v (vertex 2 5 1 '(5 6)))
 (define w (vertex 3 0 9 '(2 6)))
 (define t (vertex 1 8 1 '(5 6)))
 (define z (vertex 4 7 9 '(9 6)))
 (define l (list v w z t))
 (define l1 '(2 3 1 4))
 (define d '())
+|#
 
 ;;(initial 2 l1 '())
 
@@ -77,22 +75,20 @@
 ;;(min_dis l v 100000000000000000 w)
 
 
-(define (maj_tab_dis s d dis)                                                ;met à jour le tableau des distances
-  (if (eq? s (car (car dis)))
-      (cons (list s d) (cdr dis)) 
-      (cons (car dis) (maj_tab_dis s d (cdr dis))))         
+(define (maj_tab_dis s d liste_double)                                                ;met à jour le tableau des distances
+  (if (eq? s (caar liste_double))
+      (cons (list s d) (cdr liste_double)) 
+      (cons (car liste_double) (maj_tab_dis s d (cdr liste_double))))         
   )
 
 ;(maj_dis 1 10 '((4 . infini) (1 . infini) (3 . infini) (2 . 0))) 
 
 (define (maj_pred2 pred s1 s2)
   (map 
-      (lambda ( L ) ( if ( eq? (cadr L) s2 ) (list s1 s2) L ) )pred ))
+      (lambda (x) ( if ( eq? (cadr x) s2 ) (list s1 s2) x ) )pred ))
 
 
-<<<<<<< HEAD
 
-(define pred '((1 2)(2 4)(3 7)(4 2)(5 9)(6 2)(7 3)(8 3)(9 5)))
 ;(maj_pred2 pred 5 7)
 
 
@@ -104,48 +100,41 @@
 
 ;;(define pred '((v1 v2) (v2 v3) (v3 v4) (v5 v6) (v6 v7) (v7 v8) (v8 v9)))
 
-=======
+;(define pred '((1 1)(2 5)(3 7)(4 2)(5 2)(6 2)(7 3)(8 3)(9 10)(10 9)))
 
 
-#|
-;;(define (maj_distance sdep s1 s2 pred dis)                                  ;met à jour la valeur de la distance et le predecesseur
->>>>>>> 17b9656c9c44aa098bf49ee999f2df24a6f61729
-
-;;  (if (> ((distance dis s2) (+ (distance dis s1) (haversine (hash-ref (graph-vx-ht g) s2) (hash-ref (graph-vx-ht g) s1))))) ;; il faut faire en;; sorte que seul le vertex soit recupéré en entier
-
-<<<<<<< HEAD
-#|
 (define (maj_distance sdep s1 s2 pred dis)                                  ;met à jour la valeur de la distance et le predecesseur
 
-  (if (> ((distance dis s2) (+ (distance dis s1) (haversine (hash-ref (graph-vx-ht g) s2) (hash-ref (graph-vx-ht g) s1))))) ;; il faut faire en sorte que seul le vertex soit recupéré en entier
+  (if (> (distance dis s2) (+ (distance dis s1) (haversine (hash-ref (graph-vx-ht test) s2) (hash-ref (graph-vx-ht test) s1)))) ;; il faut faire en sorte que seul le vertex soit recupéré en entier
+      
+  ;(if (> ((distance dis s2) (+ (distance dis s1) (haversine s2 s1))))
 
-  (if (> ((distance dis s2) (+ (distance dis s1) (haversine s2 s1))))
-
-     (cons (maj_tab_dis s2 (+ (haversine sdep s2) (haversine s2 s1)) dis)
-           (maj_pred s1 s2))
-  (cons dis pred))
+      (cons (maj_tab_dis s2 (+ (haversine (hash-ref (graph-vx-ht test) sdep) (hash-ref (graph-vx-ht test) s2)) (haversine (hash-ref (graph-vx-ht test) s2) (hash-ref (graph-vx-ht test) s1))) dis)
+            (maj_pred2 s1 s2))
+      (cons dis pred))
   )
-=======
-;;  (if (> ((distance dis s2) (+ (distance dis s1) (haversine s2 s1))))
-;
- ;;    (cons (maj_tab_dis s2 (+ (haversine sdep s2) (haversine s2 s1)) dis)
-  ;;         (maj_pred s1 s2))
-  ;;(cons dis pred))
- ;; )
->>>>>>> 17b9656c9c44aa098bf49ee999f2df24a6f61729
+
+#|(define pred '((1 1)(2 5)(3 7)(4 2)(5 2)(6 2)(7 3)(8 3)(9 10)(10 9)))
+
+(define dis '((1 0)(2 248)(3 248.6)(4 400.9)(5 314.5)(6 248.6)(7 248.6)(8 400.9)(9 400.9)(10 497.2)))
+
+(maj_distance 1 3 4 pred dis)
+|#
+
 
 (define (maj_dis_voisin sdep s1 lv pred dis)
   (foldl (maj_distance sdep s1 pred dis) lv))     
 
-(define (dijkstra-loop q dis pred sdep)                                        ;q contient initialement la liste des id de tous les sommets
+(define (dijkstra-loop q dis pred sdep)                        ;q contient initialement la liste des id de tous les sommets
   (if (null? q)
       (cons dis pred)
-      ((let ((s1 (min_dis q sdep dmax sdep))
-            (q (remove s1 q))
-            (lv  (vertex-way (data s1 q ))))
-            (maj_dis_voisin sdep s1 lv)       
-       (maj_dis_voisin sdep s1 lv)
-       (dijkstra-loop q dis pred sdep)))))
+      (let* ([s1 (min_dis q sdep dmax sdep)]
+             [q (remove s1 q)]
+             [lv  (vertex-way ((hash-ref (graph-vx-ht test) s1 )))])
+             (maj_dis_voisin sdep s1 lv)       
+             (dijkstra-loop q dis pred sdep))))
+
+
 
 (define (dijkstra q sdep )
   (let
@@ -155,39 +144,15 @@
     (dijkstra-loop q dis pred sdep)))
 
 
-(define (maj_pred pred s1 s2)                                               ;le predecesseur de s2 est s1 (s1 s2) 
+#|(define (maj_pred_ancien pred s1 s2)                                               ;le predecesseur de s2 est s1 (s1 s2) 
   (cond [(eq? s2 (cdr (car pred)))
          (cons (list s1 s2) (cdr pred))]
         [else (maj_pred (cdr pred) s1 s2)])
         pred)
 
-
-
-  
-  
-  
-  
-    
-    
-  
-  
-  
-
-
-
-
-Dijkstra(G,Poids,sdeb)
-1 Initialisation(G,sdeb)
-2 Q := ensemble de tous les nœuds
-3 tant que Q n'est pas un ensemble vide faire
-4       s1 := Trouve_min(Q)
-5       Q := Q privé de s1
-6       pour chaque nœud s2 voisin de s1 faire
-7           maj_distances(s1,s2)
-8       fin pour
-9 fin tant que
-
 |#
+
+
 
 
 
